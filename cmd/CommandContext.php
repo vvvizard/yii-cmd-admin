17bb016a25202c9544aa8cb73ddff115ce8a715e
vvvizard <vvvizard@outlook.com>
2025-10-29 00:07:40 +0700

    getting params from parser

--- cmd/CommandContext.php
+++ cmd/CommandContext.php
@@ -2,31 +2,50 @@
 
 namespace app\cmd;
 
+use app\cmd\QueryParser;
 use Yii;
 
 class CommandContext
 {
-   private $params = [];
-   private $error = [];
-   
-   public function __construct(){
-         $this->params = Yii::$app->request->getBodyParams();
-   }
+      private $params = [];
+      private $error = [];
 
-   public function addParam( string $key, $val){
-    $this->params[$key] = $val;
-   }
+      protected $query;
+      public function __construct(QueryParser $query)
+      {
+              $this->initQuery($query);
+      }
 
-   public function get(string $key): string
-   {
-    return $this->params[$key] ?: null ;
-   }
+      protected function initQuery($query)
+      {
+            $this->query = $query;
+            if ($query->get('callYesNo')) {
+                  $this->params['callYesNo'] = $query->getQueryString();
+            }
+            $this->params = $this->query->parseParams();
+      }
 
-   public function setError($error){
-    $this->error = $error;
-   }
+      public function addParam(string $key, $val)
+      {
+            $this->params[$key] = $val;
+      }
 
-   public function getError(){
-    return $this->error;
-   }
+      public function get(string $key)
+      {
+            return $this->params[$key] ?? null;
+      }
+
+      public function getParams(){
+            return $this->params;
+      }
+
+      public function setError($error)
+      {
+            $this->error = $error;
+      }
+
+      public function getError()
+      {
+            return $this->error;
+      }
 }
