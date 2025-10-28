21b980bea523917bd0bda8a2b6422f70bcdf5304
vvvizard <vvvizard@outlook.com>
2025-10-29 00:06:30 +0700

    admin cmd controller

---
+++ controllers/AdminCmdController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+use app\cmd\{CommandContext, CommandFactory, Command, QueryParser};
+use Exception;
+
+
+
+
+class AdminCmdController extends Controller
+{
+
+    protected $context;
+
+    public function beforeAction($action)
+    {
+        $this->enableCsrfValidation = false;
+        return parent::beforeAction($action);
+    }
+    public function actionCmd()
+    {
+        Yii::$app->params;
+        try {
+            $data = json_decode(file_get_contents('php://input'), true);
+            $query = new QueryParser($data['cmd']);
+            $context = new CommandContext($query);
+            $cmd = CommandFactory::getCommand($query->getCommandTitle());
+            $cmd->execute($context);
+        } catch (Exception $e) {
+            $response = Yii::$app->response;
+            $response->format = \yii\web\Response::FORMAT_JSON;
+            $response->data = ['terminalMessage' => $e->getMessage()];
+        }
+    }
+}
