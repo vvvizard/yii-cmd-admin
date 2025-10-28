8fc39f7dca264be52f89c46ddc00f1c1181618cb
vvvizard <vvvizard@outlook.com>
2025-10-29 00:05:31 +0700

    add response methods

--- cmd/Command.php
+++ cmd/Command.php
@@ -2,8 +2,23 @@
 namespace app\cmd;
 
 use app\cmd\CommandContext;
+use Yii;
 abstract class Command
 {
- abstract public function execute(CommandContext $context):bool;
- 
+    abstract public function execute(CommandContext $context): bool;
+
+    protected function jsonResponse(array $data)
+    {
+        $response = Yii::$app->response;
+        $response->format = \yii\web\Response::FORMAT_JSON;
+        $response->data = $data;
+        $response->send();
+        return true;
+    }
+
+    protected function terminalMessage(string $message)
+    {
+        return ['terminalMessage' => $message];
+    }
+
 }
